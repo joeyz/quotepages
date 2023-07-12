8ebb73ad90c269ed95abac17d72270debcc7df2f
joey zielinski <joeyz5150@gmail.com>
2023-07-12 10:46:48 -0400

    ajax and some css for the page

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-$.ajax({
-   type: "get",
-   url: "https://type.fit/api/quotes",
-   data: "data",
-   dataType: "json",
-   success: function (response) {
-      console.log(response);
-   }
-});
-var quotes = [
-   "The only way to do great work is to love what you do. - Steve Jobs",
-   "In the middle of difficulty lies opportunity. - Albert Einstein",
-   "Believe you can and you're halfway there. - Theodore Roosevelt",
-   "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-   "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-   "The best way to predict the future is to create it. - Peter Drucker",
- ];
- 
- let quoteText = document.querySelector(".quote_text");
- 
- function generateQuote() {
+let quoteText = document.querySelector(".quote_text");
+let quoteAuthor = document.querySelector(".quote_author");
+let quotes = getQuotes();
+
+function generateQuote()
+{
    var randomIndex = Math.floor(Math.random() * quotes.length);
-   var quote = quotes[randomIndex]
-   quoteText.innerHTML = '"' + quote + '"'
- }
+   var quote = quotes[randomIndex];
+   quoteText.innerHTML = '"' + quote.text + '"';
+   quoteAuthor.innerHTML = '~' + quote.author;
+}
+
+function getQuotes() {
+   $.ajax({
+      async: true,
+      crossDomain: true,
+      url: "https://type.fit/api/quotes",
+      method: "GET",
+      success: function (response)
+      {
+         return quotes = JSON.parse(response);
+      }
+   });
+}
